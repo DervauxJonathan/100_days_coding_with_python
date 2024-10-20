05958736a3e03ec3c85cffb1c49273f9a4b72fc1
DERVAUX Jonathan <33451596+DervauxJonathan@users.noreply.github.com>
2024-10-20 23:36:11 +0200

    day9

---
+++ day9/day_9.py
@@ -0,0 +1,34 @@
+from art import logo
+import os
+
+print(logo)
+
+print("Welcome to the secret auction program.")
+
+finish = False
+bid_list = {}
+
+def bidding_process(bid_list):
+    highest_bid = 0
+    winner = ""
+    for bidder in bid_list:
+        bid_amount = bid_list[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not finish:
+    name = input("What is your name?: ")
+    bid = int(input("What's your bid?: $"))
+    bid_list[name] = bid
+    other_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+
+    if other_bidders not in ["yes", "no"]:
+        other_bidders = input("Are there any other bidders? Type 'yes or 'no'.\n").lower()
+    elif other_bidders == "yes":
+        os.system('cls')
+    elif other_bidders == "no":
+        finish = True
+        bidding_process(bid_list)
+
